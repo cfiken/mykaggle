52300ed1f91bf18a258f7595c993f40a4c0e6d51
cfiken <cfiken@gmail.com>
2022-07-06 01:58:32 +0900

    Add: setup_torch script

--- script/setup_torch.sh
+++ script/setup_torch.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-docker-compose run mykaggle poetry run pip install -U pip setuptools
-docker-compose run mykaggle poetry run pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+if test $CI; then
+    docker-compose run mykaggle poetry run pip install -U pip setuptools
+    poetry run pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+else
+    docker-compose run mykaggle poetry run pip install -U pip setuptools
+    docker-compose run mykaggle poetry run pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
+fi
 
