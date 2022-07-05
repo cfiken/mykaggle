bc2066c9b6a2156930f11a9366e5c8a95b3c198b
cfiken <cfiken@gmail.com>
2022-07-06 02:19:48 +0900

    Fix: circleci

--- script/setup_torch.sh
+++ script/setup_torch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if test $CI; then
-    docker-compose run mykaggle poetry run pip install -U pip setuptools
+    poetry run pip install -U pip setuptools
     poetry run pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu113
 else
     docker-compose run mykaggle poetry run pip install -U pip setuptools
