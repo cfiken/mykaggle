a8613210790e689a1398442e905f21d3926121f2
cfiken <cfiken@gmail.com>
2023-01-27 03:48:46 +0000

    Fix: competition setup shell

--- script/setup_competition.sh
+++ script/setup_competition.sh
@@ -10,8 +10,6 @@ if [ $# -ne 1 ]; then
 fi
 
 sed -i -e "s/container_name: mykaggle/container_name: $1/g" ./docker-compose.yml
-sed -i -e "s/docker-compose run --rm mykaggle/docker-compose run --rm $1/g" ./script/setup_kaggle_dataset.sh
-sed -i -e "s/docker-compose run --rm mykaggle/docker-compose run --rm $1/g" ./script/setup_torch.sh
-sed -i -e "s/docker-compose run --rm mykaggle/docker-compose run --rm $1/g" ./script/upload_ckpt.sh
+sed -i -e "s/container_name: mykaggle/container_name: $1/g" ./docker-compose-cpu.yml
 sed -i -e "s/git-mykaggle/git-mykaggle-$1/g" ./kaggle_datasets_git/dataset-metadata.json
 sed -i -e "s/ckpt-mykaggle/ckpt-mykaggle-$1/g" ./kaggle_datasets_ckpt/dataset-metadata.json
